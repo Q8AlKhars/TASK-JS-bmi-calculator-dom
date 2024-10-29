022e871da1d9d8d05e34100f41fb2fa648c0995d
Q8AlKhars <Mohammad.a.alkhars@gmail.com>
2024-10-29 20:18:18 +0300

    task done

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  // ^2 or ** 2 for square التربيع (الأس)
+
+  const heightInMeters = height / 100;
+  const BMI = weight / (heightInMeters ** 2 );
+  
+  let alertMessage = `your bmi is: ${BMI} \n`;
+  if (BMI < 18.5) {
+   alertMessage += "Underweight";
+  } else if (BMI >= 18.5 && BMI <= 24.9 ) {
+    alertMessage += "Healthy Weight";
+  } else if (BMI >= 25.0 && BMI <= 29.9 ) {
+    alertMessage += "Overweight";
+  } else if (BMI > 30.0 ) {
+    alertMessage += "Obesity";
+  }
+  alert(alertMessage);
 }
